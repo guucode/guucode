3e973e43ccb6723c9680a925853c14477d8247ac
Banlung Sirijapma <banlungtony@gmail.com>
2017-06-24 04:57:36 +0700

    add login function with email and google auth

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  user: Observable<firebase.User>;
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth,private router: Router) {
+    this.user = afAuth.authState;
+    if (this.user) this.router.navigate(['/home']);
+  }
+  /*end cons*/
+
+  logout() {
+    this.afAuth.auth.signOut();
+    this.router.navigate(['/login']);
+
+  }
+  /*end func*/
 
   ngOnInit() {
+    console.log(this.user);
   }
 
 }
+/*end class*/
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  constructor(public afAuth: AngularFireAuth,private router: Router) {
+  }
+  /*end cons*/
 
-  constructor() { }
+  emailLogin(formData) {
+    if(formData.valid) {
+      this.afAuth.auth.signInWithEmailAndPassword(formData.value.email,formData.value.password)
+        .then(
+          (success) => {
+            this.router.navigate(['/home']);
+          }).catch(
+        (err) => {
+          console.log(err);
+        })
+    }
+  }
+  /*end func*/
+
+  googleLogin() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(
+        (success) => {
+          this.router.navigate(['/home']);
+        })
+      .catch(
+      (err) => {
+        console.log(err);
+      });
+  }
+  /*end func*/
 
   ngOnInit() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) this.router.navigate(['/home']);
+    });
   }
 
 }
+/*end class*/
