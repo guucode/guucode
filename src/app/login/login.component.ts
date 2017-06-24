18e19e0baa624d2de74c137c9f2183c9a870b954
SarinPC <iamsarin94@gmail.com>
2017-06-24 11:53:03 +0700

    - rollback code login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase/app';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,45 +6,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  public email: string = "";
-  public password: string = "";
 
-  constructor(public afAuth: AngularFireAuth,private router: Router) {
+  constructor() {
   }
-  /*end cons*/
-
-  emailLogin() {
-    if(this.email && this.password) {
-      this.afAuth.auth.signInWithEmailAndPassword(this.email,this.password)
-        .then(
-          (success) => {
-            this.router.navigate(['/home']);
-          }).catch(
-        (err) => {
-          console.log(err);
-        })
-    }
-  }
-  /*end func*/
-
-  googleLogin() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
-      .then(
-        (success) => {
-          this.router.navigate(['/home']);
-        })
-      .catch(
-      (err) => {
-        console.log(err);
-      });
-  }
-  /*end func*/
 
   ngOnInit() {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) this.router.navigate(['/home']);
-    });
   }
 
 }
-/*end class*/
