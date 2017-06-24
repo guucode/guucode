f3a41508b2da74ae7c96d579749501a111d61c4e
Priefy <peachy-lovly-mom@hotmail.com>
2017-06-25 01:53:49 +0700

    add on error image source & remove user image on list-item page

--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFireDatabase} from 'angularfire2/database';
+import {AngularFireAuth} from 'angularfire2/auth';
+import {Router} from '@angular/router';
+import {MdDialog} from '@angular/material';
 
 @Component({
   selector: 'app-list-item',
@@ -7,7 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListItemComponent implements OnInit {
 
-  constructor() {
+  public uid = '';
+  public email = '';
+  public displayName = '';
+  public photoURL = '';
+  // public user: Observable<firebase.User>;
+  // public userInfo: Observable<firebase.UserInfo>;
+
+  constructor(private router: Router) {
+    if (!('uid' in localStorage)) {
+      this.router.navigate(['/login']);
+      return;
+    }
+    this.uid = localStorage.getItem('uid');
+    this.email = localStorage.getItem('email');
+    this.displayName = localStorage.getItem('displayName');
+    this.photoURL = localStorage.getItem('photoURL');
   }
 
   ngOnInit() {
