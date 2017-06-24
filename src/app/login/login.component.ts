102782a7d55a929bfe21086c9fd32251e3b407a7
Banlung Sirijapma <banlungtony@gmail.com>
2017-06-24 12:41:19 +0700

    [SignUp] add signUp function

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +9,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public email: string = "";
+  public password: string = "";
 
-  constructor() {
+  constructor(public afAuth: AngularFireAuth, private router: Router) {
   }
+
+  /*end cons*/
+
+  emailLogin() {
+    if (this.email && this.password) {
+      this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password)
+        .then(
+          (success) => {
+            // The firebase.User instance:
+            var user = success.user;
+            // The Facebook firebase.auth.AuthCredential containing the Facebook
+            // access token:
+            var credential = success.credential;
+            this.router.navigate(['/home']);
+          }).catch(
+        (err) => {
+          console.log(err);
+          alert('Username or Password is invaild');
+        })
+    }
+  }
+
+  /*end func*/
+
+  facebookLogin() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+      .then(
+        (success) => {
+          this.router.navigate(['/home']);
+        })
+      .catch(
+        (err) => {
+          console.log(err);
+        });
+  }
+
+  /*end func*/
+
+  googleLogin() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(
+        (success) => {
+          this.router.navigate(['/home']);
+        })
+      .catch(
+        (err) => {
+          console.log(err);
+        });
+  }
+
+  /*end func*/
 
   ngOnInit() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) this.router.navigate(['/home']);
+    });
   }
 
+  /*end func*/
+
 }
+/*end class*/
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  public email: string = '';
+  public password: string = '';
+  public rePassword: string = '';
 
-  constructor() { }
+  constructor(public af: AngularFireAuth, private router: Router) {
+  }
+
+  /*end func*/
+
+  onSubmit() {
+    if (this.email && this.password && this.rePassword) {
+      if (this.password == this.rePassword) {
+        this.af.auth.createUserWithEmailAndPassword(this.email, this.password)
+          .then(
+            (success) => {
+              console.log(success);
+              this.router.navigate(['/home'])
+            }).catch(
+          (err) => {
+            console.log(err);
+            alert('ลงทะเบียนแล้ว');
+          });
+      } else {
+        alert('password ไม่ตรงกัน');
+      }
+    }
+  }
+
+  /*end func*/
 
   ngOnInit() {
   }
 
+  /*end func*/
+
 }
+/*end class*/
