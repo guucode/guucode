fa0919e9834de04002d87587caa6a951de41c210
Priefy <peachy-lovly-mom@hotmail.com>
2017-06-24 16:26:21 +0700

    separate view income&expense

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,7 @@ const appRoutes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'sign-up', component: SignUpComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'list-item', component: ListItemComponent},
+  {path: 'list-item/:type', component: ListItemComponent},
   {path: 'fb', component: FirebaseLoginComponent},
   {path: '**', component: LoginComponent}
 ];
--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-list-item',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-item.component.css']
 })
 export class ListItemComponent implements OnInit {
+  type = '';
+  type_text = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this.type = this.route.snapshot.params['type']
+
+    if (this.type === 'income') {
+      this.type_text = 'รายรับ';
+    } else if (this.type === 'expense') {
+      this.type_text = 'รายจ่าย';
+    }
   }
 
 }
