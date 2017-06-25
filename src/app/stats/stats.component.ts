712781f6d0d6fcd460f43427f2fab87c85382bc8
Priefy <peachy-lovly-mom@hotmail.com>
2017-06-25 16:18:28 +0700

    display stat from DB

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -6,23 +6,60 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./stats.component.css']
 })
 export class StatsComponent implements OnInit {
+  public month_th: Array<any> = [
+    'มกราคม',
+    'กุมภาพันธ์',
+    'มีนาคม',
+    'เมษายน',
+    'พฤษภาคม',
+    'มิถุนายน',
+    'กรกฎาคม',
+    'สิงหาคม',
+    'กันยายน',
+    'ตุลาคม',
+    'พฤศจิกายน',
+    'ธันวาคม'
+  ];
+
+  date = new Date();
+  year = this.date.getFullYear();
+  month = this.date.getMonth();
+
+  stat = '';
+  stat_arr: Array<any>;
   public barChartOptions: any = {
     scaleShowVerticalLines: false,
     responsive: true
   };
-  public barChartLabels: string[] = ['เมษายน', 'พฤษภาคม', 'มิถุนายน'];
+  public barChartLabels: string[];
   public barChartType = 'bar';
   public barChartLegend = true;
 
-  public barChartData: any[] = [
-    {data: [65, 59, 80], label: 'รายรับ'},
-    {data: [28, 48, 40], label: 'รายจ่าย'}
-  ];
+  public barChartData: any[];
 
   constructor() {
   }
 
   ngOnInit() {
+    this.stat = localStorage.getItem('stat');
+    this.stat_arr = JSON.parse(this.stat);
+
+    this.barChartData = [
+      {
+        data: [
+          this.stat_arr['income'][this.year][+this.month - 1],
+          this.stat_arr['income'][this.year][+this.month],
+          this.stat_arr['income'][this.year][+this.month + 1]], label: 'รายรับ'
+      },
+      {
+        data: [
+          this.stat_arr['payment'][this.year][+this.month - 1],
+          this.stat_arr['payment'][this.year][+this.month],
+          this.stat_arr['payment'][this.year][+this.month + 1]], label: 'รายจ่าย'
+      }
+    ];
+
+    this.barChartLabels = [this.month_th[+this.month - 2], this.month_th[+this.month - 1], this.month_th[+this.month]];
   }
 
 }
